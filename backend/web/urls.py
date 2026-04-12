9226a3466add42f876687df6a124f0d4873856e8
shihao zhang <1403287045@qq.com>
2026-04-12 15:55:59 +0800

    实现首页前后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
 
 from web.views.create.character.create import CreateCharacterView
+from web.views.create.character.get_list import GetListCharacterView
 from web.views.create.character.get_single import GetSingleCharacterView
 from web.views.create.character.remove import RemoveCharacterView
 from web.views.create.character.update import UpdateCharacterView
+from web.views.homepage.index import HomepageIndexView
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
@@ -24,6 +26,8 @@ urlpatterns = [
     path('api/create/character/update/', UpdateCharacterView.as_view()),
     path('api/create/character/remove/', RemoveCharacterView.as_view()),
     path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
+    path('api/create/character/get_list/', GetListCharacterView.as_view()),
+    path('api/homepage/index/', HomepageIndexView.as_view()),
 
     path('', index),
 
